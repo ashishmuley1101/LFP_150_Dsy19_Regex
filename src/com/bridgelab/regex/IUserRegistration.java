d1dbfe9bf505d73392a919aa1947cdb8c9d15c0d
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-07-03 16:13:20 +0530

    User Registration all UC

---
+++ src/com/bridgelab/regex/IUserRegistration.java
@@ -0,0 +1,15 @@
+package com.bridgelab.regex;
+
+public interface IUserRegistration {
+
+	public boolean validateFName(String fName);
+
+	public boolean validateLname(String LName);
+
+	public boolean valdateEmail(String email);
+
+	public boolean validatephoneNumber(String phone);
+
+	public boolean validatePwd(String pwd);
+
+}
